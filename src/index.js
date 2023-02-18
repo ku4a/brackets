7c1cc3c442e7bc1eb7e1c9c1c5a1b48dc1df416d
ku4a <7916884@gmail.com>
2023-02-18 21:20:31 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let rules = {};
+    let stack = [];
+
+    bracketsConfig.forEach(function (item) {
+        rules[item[1]] = item[0];
+    });
+    for (let char of str) {
+        if (char in rules) {
+            if (char === rules[char]) {
+                if (stack[stack.length - 1] === char) {
+                    stack.pop();
+                } else {
+                    stack.push(char);
+                }
+            } else {
+                if (stack.pop() !== rules[char]) {
+                    return false;
+                }
+            }
+        } else {
+            stack.push(char)
+        }
+    }
+    return stack.length <= 0;
 }
